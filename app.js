b7af30666185bf5a30e9118a39d8d9aa851c2f69
blazepk <pratyushkk880@gmail.com>
2023-03-10 20:05:34 +0530

    test

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 const http = require("http");
 
-http.createServer(function(req, res) {
+http
+  .createServer(function (req, res) {
+    res.write("On the way to being a full snack engineer! yipeee");
+    res.end();
+  })
+  .listen(3000);
 
-res.write("On the way to being a full snack engineer!")
-res.end()	
-
-
-
-
-}).listen(3000)
-
-console.log("Server Started")
+console.log("Server Started");
